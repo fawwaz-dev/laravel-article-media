4d7d779a2ab2ef0aea4936178f03b815b8c1fb56
fawwaz-dev <fawwazalfarisi.works@gmail.com>
2025-05-28 14:23:32 +0700

    Feat(web): Add web route for auth

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,24 @@
 <?php
 
 use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
+
+Route::middleware('guest')->group(function () {
+    Route::get('/register', [AuthController::class, 'indexRegister'])->name('auth.register');
+    Route::get('/login', [AuthController::class, 'indexLogin'])->name('auth.login');
+    Route::post('/register', [AuthController::class, 'register'])->name('register');
+    Route::post('/login', [AuthController::class, 'login'])->name('login');
+});
+
+Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
-
-Route::resource('articles', ArticleController::class);
+Route::group(['middleware' => 'auth'], function () {
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+    Route::resource('articles', ArticleController::class);
+});
